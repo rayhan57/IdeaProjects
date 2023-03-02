b57e9a05729d1f12e426e6c92ff07317c4cc90d5
rayhan57 <rayhanb18@gmail.com>
2023-03-02 18:23:20 +0700

    tambah FunctionNamedArgument

---
+++ BelajarKotlin/src/main/kotlin/FunctionNamedArgument.kt
@@ -0,0 +1,7 @@
+fun namaPanjang(namaDepan: String, namaTengah: String, namaBelakang: String) {
+    println("Hai $namaDepan $namaTengah $namaBelakang")
+}
+
+fun main() {
+    namaPanjang(namaTengah = "Lingga", namaDepan = "Rayhan", namaBelakang = "Buana")
+}
